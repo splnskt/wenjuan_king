3e7fdf32769a8d410504017830523a3e6c230092
qwe <1604196971@qq.com>
2024-05-30 16:40:03 +0800

    创建项目

---
+++ src/main/java/com/scut626/wenjuan_king/WenjuanKingApplication.java
@@ -0,0 +1,13 @@
+package com.scut626.wenjuan_king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenjuanKingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenjuanKingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scut626/wenjuan_king/WenjuanKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut626.wenjuan_king;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WenjuanKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
