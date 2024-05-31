98587ccd8c7dbbb965af9b96c462b89a6be748fe
HongmaoMaster <211058692@qq.com>
2024-05-31 21:29:24 +0800

    注册

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -8,7 +8,7 @@ function checkPasswordStrength() {
         return;
     }
 
-    // 强度级别及对应的颜色和描述
+
     var strength = {
         0: "很弱",
         1: "较弱",
@@ -55,57 +55,36 @@ function validateForm() {
     }
     return true;
 }
-function checkPasswordStrength() {
-    var password = document.getElementById("password").value;
-    var strengthBadge = document.getElementById("password-strength");
+document.addEventListener("DOMContentLoaded",function(event){
+   var app=new Vue({
+    el:'#register',
+    data:{
+        username:'',
+        password:''
+    },
+    
+    methods:{
+        register:function()
+        {
+            var formData=new FormData();
+            formData.append('username',this.username);
+            formData.append('password',this.password);
+            console.log('用户名：', this.username);
+            console.log('密码：', this.password);
+           axios.post('/user/register',formData)
+            .then(response=>{
+                if(response.data.code==='0')
+                    window.location.href = '../pages/login.html';
+                else{
+                    alert("用户名被占用!尝试其他用户名！");
+                }
 
-    // 如果密码为空，则不显示强度提示
-    if (password === "") {
-        strengthBadge.style.display = "none";
-        return;
-    }
+            })
+            
 
-    // 检查密码长度
-    if (password.length < 6 || password.length > 18) {
-        strengthBadge.style.display = "block";
-        strengthBadge.innerText = "密码长度应在6至18位之间";
-        strengthBadge.className = "strength-0";
-        return;
-    }
+        }
 
-    // 强度级别及对应的颜色和描述
-    var strength = {
-        0: "很弱",
-        1: "较弱",
-        2: "中等",
-        3: "较强",
-        4: "很强"
-    };
-
-    // 密码强度
-    var strengthLevel = 0;
-
-    // 如果密码长度大于 8 个字符，则强度加 1
-    if (password.length > 8) {
-        strengthLevel++;
     }
-
-    // 如果密码中包含大写字母、小写字母、数字和特殊字符中的任意一个，则强度加 1
-    if (password.match(/[a-z]+/)) {
-        strengthLevel++;
-    }
-    if (password.match(/[A-Z]+/)) {
-        strengthLevel++;
-    }
-    if (password.match(/[0-9]+/)) {
-        strengthLevel++;
-    }
-    if (password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/)) {
-        strengthLevel++;
-    }
-
-    // 设置强度级别及对应的颜色和描述
-    strengthBadge.style.display = "block";
-    strengthBadge.innerText = strength[strengthLevel];
-    strengthBadge.className = "strength-" + strengthLevel;
+   })
 }
+);
