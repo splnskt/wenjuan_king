b9c65ef1f6351947e754f33631199cdd6bd545e9
RANN000 <1170883985@qq.com>
2024-06-08 00:47:37 +0800

    删除问卷功能实现

--- src/main/resources/static/js/zone.js
+++ src/main/resources/static/js/zone.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
         el: '#app',
         data: {
             currentComponent: '', //当前选择的模块,未设置默认界面
-            papers:[],
+            papers: [],
             currentPage: 1,
             totalPages: 1,
-            pageSize: 1
+            pageSize: 1,
+            batchProcessing: false, // 是否处于批量处理状态
+            pidList: [],
         },
         methods: {
             //请求我的问卷列表
@@ -31,10 +33,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 }
             },
             // 显示我的问卷列表
-            showPapers(){
-                currentPage=1;
+            showPapers() {
+                this.currentPage = 1;
                 this.fetchMyPapers(this.currentPage);
-                this.currentComponent='myPapers';
+                this.currentComponent = 'myPapers';
             },
             prevPage() {
                 if (this.currentPage > 1) {
@@ -48,15 +50,44 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     this.fetchMyPapers(this.currentPage);
                 }
             },
+            // 填写问卷
             fillPaper(pid) {
                 // 跳转到填写问卷页面，并传递问卷ID
                 window.location.href = '../pages/surveyFill.html?pid=' + pid;
             },
-            modifyPaper(pid){
+            // 修改问卷
+            modifyPaper(pid) {
                 window.location.href = '../pages/surveyModify.html?pid=' + pid;
             },
-            showOthers(){
-                this.currentComponent='others';
+            // 批量管理
+            changeBatch() {
+                this.batchProcessing = !this.batchProcessing;
+            },
+            // 删除问卷
+            deletePapers() {
+                if (this.pidList.length === 0) {
+                    alert('请选择要删除的问卷！');
+                    return;
+                };
+                var formData = this.pidList;
+                axios.post('/paper/delete-paper', formData)
+                    .then(response => {
+                        console.log(response.data);
+                        if (response.data.data === 0) {
+                            alert('删除成功！');
+                            // 清空选中的问卷数组
+                            this.pidList = [];
+                            // 重新显示问卷列表
+                            this.showPapers();
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error deleting papers:', error);
+                    });
+            },
+            // 其他选项，待编辑
+            showOthers() {
+                this.currentComponent = 'others';
             }
         },
 
