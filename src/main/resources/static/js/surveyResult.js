0f72f30ae6b78df98de8da31df0c91943afcf7e0
RANN000 <1170883985@qq.com>
2024-06-15 22:20:52 +0800

    查看结果

---
+++ src/main/resources/static/js/surveyResult.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            surveyData: null,
+            //id暂未获取
+            pid: ''
+        },
+        methods: {
+            fetchData() {
+                var pid = this.pid;
+                axios.post('/answer/paper-data', pid)
+                    .then(response => {
+                        console.log(response.data);
+                        this.surveyData = response.data;
+                        this.surveyData.data.questions.forEach(question => {
+                            question.isShowAnswers = false;  // 初始设置为未展开状态
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error deleting papers:', error);
+                    });
+            },
+            showAnswers(question) {
+                question.isShowAnswers = !question.isShowAnswers;
+            },
+        },
+        mounted() {
+            const urlParams = new URLSearchParams(window.location.search);
+            this.pid = urlParams.get('pid');
+            if (!this.pid) {
+                console.error('未提供问卷ID');
+            }
+            this.fetchData();
+        }
+    })
+});
